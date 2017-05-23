45df7e2e82e78488a7cd7f7ef898260e0d6c77c4
Ithalis <jt.2012@yahoo.com>
2017-05-23 11:37:02 -0700

    scss has been messed with

--- lab-james/app/entry.js
+++ lab-james/app/entry.js
@@ -19,7 +19,7 @@ function CowsayController($log, $scope) {
 
   cowsayCtrl.speak = function(input) {
     $log.debug('#cowsayCtrl.speak');
-    return cowsay.say({text: input || '*dragon*', f: 'dragon'});
+    return cowsay.say({text: input || '*roar*', f: 'dragon'});
   };
 
   cowsayCtrl.logger = function(input) {
