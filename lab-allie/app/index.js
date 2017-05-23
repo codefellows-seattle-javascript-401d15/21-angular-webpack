6d989a8238a8ae1aad97f9bd67728ed46b2d29ff
Allie Grampa <agrampa@yahoo.com>
2017-05-23 09:31:58 -0700

    second turtle shows but does not have same text yet

--- lab-allie/app/index.js
+++ lab-allie/app/index.js
@@ -15,16 +15,24 @@ function CowsayController($log, $scope) {
   
   let cowsayCtrl = $scope.cowsayCtrl = {};
   
-  cowsayCtrl.title = 'Got a fever?';
+  cowsayCtrl.title = 'I like turtles.';
   
   cowsayCtrl.speak = function(input) {
     $log.debug('#cowsayCtrl.speak');
     
-    return cowsay.say({text: input || 'I\'m Bruce Dickinson', f: 'goat'});
+    return cowsay.say({text: input || 'I like turtles.', f: 'turtle'});
   }
   
   cowsayCtrl.logger = function(input) {
     $log.debug('#cowsayCtrl.logger');
     $log.log(input);
+  }
+  
+  cowsayCtrl.showSecond = function() {
+    $log.debug('#cowsayCtrl.showSecond');
+    // document.getElementById('second').style.visibility = visible;
+    // document.getElementById('second').setAttribute('visibility', 'visible');
+    document.getElementById('second').classList.remove('second');
+    
   }
 }
