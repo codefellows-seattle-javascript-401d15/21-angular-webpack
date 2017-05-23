e2e02da5e37ec9dc79df9e9ba2719861f6b39560
Allie Grampa <agrampa@yahoo.com>
2017-05-22 17:53:49 -0700

    cow works

---
+++ lab-allie/app/index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+// require('./scss/reset.scss');
+// require('./scss/main.scss');
+
+const angular = require('angular');
+const cowsay = require('cowsay-browser');
+
+const cowsayApp = angular.module('cowsayApp', []);
+
+cowsayApp.controller('CowsayController', ['$log', '$scope', CowsayController]);
+
+function CowsayController($log, $scope) {
+  $log.debug('#CowsayController');
+  
+  let cowsayCtrl = $scope.cowsayCtrl = {};
+  
+  cowsayCtrl.title = 'Got a fever?';
+  
+  cowsayCtrl.speak = function(input) {
+    $log.debug('#cowsayCtrl.speak');
+    
+    return cowsay.say({text: input || 'I\'m Bruce Dickinson', f: 'goat'});
+  }
+  
+  cowsayCtrl.logger = function(input) {
+    $log.debug('#cowsayCtrl.logger');
+    $log.log(input);
+  }
+}
